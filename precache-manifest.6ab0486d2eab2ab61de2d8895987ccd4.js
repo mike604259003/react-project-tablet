25d6753c94da6a99248bb63b6f2dc75cfe7e1dd7
Mike <604259003@webmail.npru.ac.th>
2020-08-05 23:56:41 +0700

    Updates

---
+++ precache-manifest.6ab0486d2eab2ab61de2d8895987ccd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7300a5db793360d7c56acd757249be",
+    "url": "/react-project-tablet/index.html"
+  },
+  {
+    "revision": "273dc9712330d7267a7a",
+    "url": "/react-project-tablet/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2a5be48fd424366091a8",
+    "url": "/react-project-tablet/static/css/main.aa90a491.chunk.css"
+  },
+  {
+    "revision": "273dc9712330d7267a7a",
+    "url": "/react-project-tablet/static/js/2.184509c0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-project-tablet/static/js/2.184509c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5be48fd424366091a8",
+    "url": "/react-project-tablet/static/js/main.bdfcebe7.chunk.js"
+  },
+  {
+    "revision": "9da4d528d41a880deb85",
+    "url": "/react-project-tablet/static/js/runtime-main.6dda6dc6.js"
+  }
+]);
